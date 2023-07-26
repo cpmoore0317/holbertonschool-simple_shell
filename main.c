4e422a83966074dd285fe466af7e86c86683c0b0
“cpmoore0317” <kingpkayy@gmail.com>
2023-07-26 13:53:08 -0700

    Setting up astrcpy sequence, a token counter, and an argv statement

--- main.c
+++ main.c
@@ -8,11 +8,12 @@
 
 int main(int ac, char **argv, char **env)
 {
-	char *user_input, *token;
+	char *user_input, *user_input_copy, *token;
 	size_t bytes_read = 0;
 	ssize_t nchars_read = 0;
+	int num_tokens = 0, i;
 
-	(void)ac; (void)argv; (void)env;
+	(void)ac; (void)env;
 
 	while (1)
 	{
@@ -23,12 +24,27 @@ int main(int ac, char **argv, char **env)
 			printf("Exiting Shell...");
 			return (-1);
 		}
+		user_input_copy = malloc(sizeof(char) * nchars_read);
+		strcpy(user_input_copy, user_input);
 		token = strtok(user_input, " ");
-		argv = malloc(sizeof(c));
+		while (token != NULL)
+		{
+			num_tokens++;
+			token = strtok(NULL, " ");
+		}
+		num_tokens++;
+		argv = malloc(sizeof(char *) * num_tokens);
+		token = strtok(user_input_copy, " ");
+		for (i = 0; token != NULL; i++)
+		{
+			argv[i] = malloc(sizeof(char) * strlen(token));
+			strcpy(argv[i], token);
 
+			token = strtok(NULL, " ");
+		}
+		argv[i] = NULL;
 
 		free(user_input);
 	}
-
 	return (0);
 }
