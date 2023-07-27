ae4542e3e0ea8f1109ab8afe89dfd90aa8f5cec6
“cpmoore0317” <kingpkayy@gmail.com>
2023-07-27 09:30:04 -0700

    Adjusting the argv loop

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 
 int main(int ac, char **argv, char **env)
 {
-	char *user_input, *user_input_copy, *token;
+	char *user_input = NULL, *user_input_copy = NULL, *token;
 	size_t bytes_read = 0;
 	ssize_t nchars_read = 0;
 	int num_tokens = 0, i;
@@ -21,20 +21,25 @@ int main(int ac, char **argv, char **env)
 		nchars_read = getline(&user_input, &bytes_read, stdin);
 		if (nchars_read == -1)
 		{
-			printf("Exiting Shell...");
+			printf("Exiting Shell...\n");
 			return (-1);
 		}
+
 		user_input_copy = malloc(sizeof(char) * nchars_read);
 		strcpy(user_input_copy, user_input);
+
 		token = strtok(user_input, " ");
+
 		while (token != NULL)
 		{
 			num_tokens++;
 			token = strtok(NULL, " ");
 		}
 		num_tokens++;
+
 		argv = malloc(sizeof(char *) * num_tokens);
 		token = strtok(user_input_copy, " ");
+
 		for (i = 0; token != NULL; i++)
 		{
 			argv[i] = malloc(sizeof(char) * strlen(token));
@@ -45,6 +50,7 @@ int main(int ac, char **argv, char **env)
 		argv[i] = NULL;
 
 		free(user_input);
+		free(user_input_copy);
 	}
 	return (0);
 }
