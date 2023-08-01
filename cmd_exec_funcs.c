75b29d92f759cba9aac19e9ee9ee23ef3a45a927
Jtownokie <6673@holbertonstudents.com>
2023-08-01 12:20:41 -0700

    Minor Format Fixes

--- cmd_exec_funcs.c
+++ cmd_exec_funcs.c
@@ -42,16 +42,13 @@ char *get_location(char *command)
 	if (path)
 	{
 		path_copy = strdup(path);
-
 		command_length = strlen(command);
-
 		path_token = strtok(path_copy, ":");
 
-		while(path_token != NULL)
+		while (path_token != NULL)
 		{
 			directory_length = strlen(path_token);
 			file_path = malloc(command_length + directory_length + 2);
-
 			strcpy(file_path, path_token);
 			strcat(file_path, "/");
 			strcat(file_path, command);
@@ -69,7 +66,6 @@ char *get_location(char *command)
 			}
 		}
 		free(path_copy);
-
 		if (stat(command, &buffer) == 0)
 		{
 			return (command);
