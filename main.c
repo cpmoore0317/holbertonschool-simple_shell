e3607576d03c9c8908fadfd4cbed600d12bd1ef7
“cpmoore0317” <kingpkayy@gmail.com>
2023-08-02 11:36:34 -0700

    exit_success for return

--- main.c
+++ main.c
@@ -32,7 +32,7 @@ int main(int ac, char **argv, char **env)
 		if (tokenized_input == NULL)
 		{
 			free(tokenized_input);
-			return (255);
+			return (EXIT_SUCCESS);
 		}
 
 		cmd_exec(tokenized_input);
