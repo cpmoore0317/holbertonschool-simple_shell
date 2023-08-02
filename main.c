96aefe1bccce41afdb29dbf9f4c1019554c4bef6
“cpmoore0317” <kingpkayy@gmail.com>
2023-08-02 11:50:20 -0700

    Testing exit success for exit func if getline fails

--- main.c
+++ main.c
@@ -32,7 +32,7 @@ int main(int ac, char **argv, char **env)
 		if (tokenized_input == NULL)
 		{
 			free(tokenized_input);
-			exit(EXIT_FAILURE);
+			exit(EXIT_SUCCESS);
 		}
 
 		cmd_exec(tokenized_input);
