4076e5cdde539f54be60286469c05e2fdc75ad0d
Jtownokie <6673@holbertonstudents.com>
2023-08-02 11:53:35 -0700

    Reverting error status to EXIT_FAILURE

--- main.c
+++ main.c
@@ -32,7 +32,7 @@ int main(int ac, char **argv, char **env)
 		if (tokenized_input == NULL)
 		{
 			free(tokenized_input);
-			exit(EXIT_SUCCESS);
+			exit(EXIT_FAILURE);
 		}
 
 		cmd_exec(tokenized_input);
